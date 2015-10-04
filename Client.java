90bee4c49d448dd44d6ef82d3bd37f5de020112b
dillonmabry <dcmabry@gmail.com>
2015-10-04 02:03:48 -0400

    Updated JavaDocumentation

--- Client.java
+++ Client.java
@@ -9,43 +9,52 @@ import java.net.*;
 import java.util.*;
 
 /**
- *
+ * Client class to manage client side connections/ports
  * @author Dillon
  */
 public class Client {
     public static void main(String[] args) throws IOException {
         try {
+            /* create new client socket with open port */
             Socket clientSocket = new Socket("localhost", 4000);
-            try {       
+            try {
+                /* create out and in with outputstream and inputstream */
                 PrintWriter out = new PrintWriter(
                         clientSocket.getOutputStream(),true);;
                 BufferedReader in = new BufferedReader(new InputStreamReader
                         (clientSocket.getInputStream()));
-                Scanner inStream = new Scanner(in);
-                System.out.println("Enter name: ");
+                /* create stream scanner and normal scanner */
+                Scanner inStream = new Scanner(in); 
                 Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter name: ");
+                /*player enters name */
                 String player = scanner.nextLine();
                 out.println(player);
                 String choice;
                 do {
                     System.out.println("Play rock, paper, or scissors: ");
-                    Scanner scanner_ = new Scanner(System.in);
-                    choice = scanner_.nextLine();
+                    Scanner choiceScanner = new Scanner(System.in);
+                    /* player chooses rock, paper, or scissors */
+                    choice = choiceScanner.nextLine();
                     out.println(choice);
-
+                    /* get the server choice */
                     String serverChoice = inStream.nextLine();
                     System.out.println("Player move: "+choice+
                             " Server move: "+serverChoice);
                     String winner = inStream.nextLine();
+                    /* if the result is a draw */
                     if(winner.equals("Draw")) {
                         System.out.println("Draw! Re-play!");
                     } else {
                         System.out.println(winner);
                     }
+                /* perform only while server side has next input */
                 } while(inStream.hasNextLine());
+              /* close socket when finished */
             } finally {
                 clientSocket.close();
             }
+         /* catch any exceptions */
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Server.java
+++ Server.java
@@ -1,15 +1,15 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 /**
- *
+ * Server class to manage server connections/sockets
  * @author Dillon
  */
 public class Server {
@@ -21,35 +21,45 @@ public class Server {
      */
     public static void main(String[] args) throws IOException {
       try {
+          /* create new server socket on open port */
           ServerSocket serverSocket = new ServerSocket(4000);
+          /* accept the client socket connection */
           Socket clientSocket = serverSocket.accept();
         try {
+            /* create in and out using inputstream and outputstream */
             BufferedReader in = new BufferedReader(new InputStreamReader
                 (clientSocket.getInputStream()));
             Scanner scanner = new Scanner(in);
             PrintWriter out = new PrintWriter(
                     clientSocket.getOutputStream(), true);
+            /* get the server move for game */
             String serverMove = getMove();
             out.println(serverMove);
-            
+            /* get the player name */
             PLAYER_NAME = scanner.nextLine();
             System.out.println(PLAYER_NAME);
-            
+            /* get the player move */
             String playerMove = scanner.nextLine();
             System.out.println(playerMove);
-            
+            /* determine the round winner and put to printwriter */
             String roundWin = winner(playerMove, serverMove);
             out.println(roundWin);
-            
+            /* close the socket when finished */
             } finally {
                 clientSocket.close();
             }    
+        /* catch any exceptions */
         } catch(IOException e) {
           e.printStackTrace();
         }
     }
     
+    /** 
+     * Method to get the move of the server
+     * @return the move of the server (rock, paper, or scissors)
+     */
     public static String getMove() {
+        /* create random num generator with 3 options */
         Random random = new Random();
         int randomNum = random.nextInt((3-0)+0);
         String move = null;   
@@ -63,6 +73,12 @@ public class Server {
         return move;
     }
     
+    /**
+     * Method to determine winner between the server and player
+     * @param playerMove the player move
+     * @param serverMove the server move
+     * @return the print statement showing the winner
+     */
     public static String winner(String playerMove, String serverMove) {
         String winner;
         if(serverMove.equals(playerMove)) {
